fe87c949e0b04e5422aaf679ca14c1a6142db8a6
corlene-r <ccarirhoades@gmail.com>
2019-06-08 23:43:42 -0400

    lab8 some edits - map displays but code in index.html (this is a bad thingy :~{ )

--- notuber/notuber.js
+++ notuber/notuber.js
@@ -1,6 +1,30 @@
-var map;
-function initMap() {
-	map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: 42.352271, lng: -71.05524200000001}, zoom: 9
+function initMap()
+{
+	// Faneuil Hall
+	var map_center = new google.maps.LatLng(42.352271, -71.05524200000001);
+	
+	var a_car = new google.maps.LatLng(42.3453, -71.0464);
+	// Set up map
+	var myOptions = {
+		zoom: 13, // The larger the zoom number, the bigger the zoom
+		center: map_center,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+				
+	// Create the map in the "map_canvas" <div>
+	var map = new google.maps.Map(document.getElementById("map"), myOptions);
+	// Create a marker				
+	var marker = new google.maps.Marker({
+		position: a_car,
+		title: "mXfkjrFw"
+	});
+	marker.setMap(map);
+	// This is a global info window...
+	var infowindow = new google.maps.InfoWindow();
+				
+	// Open info window on click of marker
+	google.maps.event.addListener(marker, 'click', function() {
+		infowindow.setContent(marker.title);
+		infowindow.open(map, marker);
 	});
 }
