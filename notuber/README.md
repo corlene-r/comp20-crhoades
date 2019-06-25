13fe58328e47892a7ab71c1d16480d53c31d8a6a
corlene-r <ccarirhoades@gmail.com>
2019-06-25 19:57:21 -0400

    lab 10 finished - thanks for the help prof! I really appreciated it ;D

--- notuber/README.md
+++ notuber/README.md
@@ -1,17 +1,21 @@
 # GITHUB: corlene-r
 #
-# WORKING PARTS: I think it all works so that makes me a happy girl
+# ------PART I--------------------------------------------------------------------------------
 #
-# COLLABORATION: I did ask a few questions on piazza (and used the API you provided for this lab) but didn't talk to anyone else about it (other that showing my un-web-programming-experienced friend the cool thingy I did)
+# WORKING PARTS: 
+	- I think it all works so that makes me a happy girl
+# COLLABORATION: 
+	-I did ask a few questions on piazza (and used the API you provided for this lab) but didn't talk to anyone else about it (other that showing my un-web-programming-experienced friend the cool thingy I did)
+# TIME: 
+	- Probably about 1.5 hours in total
+# ------PART II------------------------------------------------------------------------------- 
 #
-# TIME: Probably about 1.5 hours in total
-#
-# PART II: 
-#
-# WORKING PARTS: I dunno yet :)
-#
-# TIME: Weird so i dunno... probably like 2 hours
-#
+# WORKING PARTS: 
+	- It all works! The polyline and vehicle distance is found, and I even added the pop-up
+# TIME: 
+	- Probably like 3 hours I think. Working on it has been awkwardly on/off so I don't have a really good estimate
 # COLLABORATION: None with other humans, but I did use a few websites, credited below:
     - For help with Polyline: https://www.sitepoint.com/create-a-polyline-using-the-geolocation-and-the-google-maps-api/ 
+    - More polyline stuff: https://developers.google.com/maps/documentation/javascript/examples/polyline-simple
     - Course's Piazza (It was helpful - since I'm a bit late to the party, everyone asked all the questions I needed to ask!)
+    - Also, used google's maps API and reference for it (duh)
--- notuber/notuber.js
+++ notuber/notuber.js
@@ -6,7 +6,7 @@ var map;
 var cars = []; var closest_car = {}; var car_pic = 'car.png';
 var map_center = new google.maps.LatLng(42.352271, -71.05524200000001);
 var myOptions = {
-	zoom: 13, 
+	zoom: 12, 
 	center: map_center,
 	mapTypeId: google.maps.MapTypeId.ROADMAP
 };
@@ -17,57 +17,45 @@ function initMap() {
 	renderMap();
 }
 
-function getMyLocation() {
-	if (navigator.geolocation) { // the navigator.geolocation object is supported on your browser
-			navigator.geolocation.getCurrentPosition(function(position) {
-				user_lat = position.coords.latitude; user_lng = position.coords.longitude;
-				user = new google.maps.LatLng(user_lat, user_lng);
-			});
-		}
-		else {
-			alert("No geolocation. This won't work :/");
-		}
-}
-
-
 function renderMap() {
 	if (navigator.geolocation) { // the navigator.geolocation object is supported on your browser
 		navigator.geolocation.getCurrentPosition(function(position) {
 			user_lat = position.coords.latitude; user_lng = position.coords.longitude;
 			user = new google.maps.LatLng(user_lat, user_lng);
-		});
 
-		console.log("username=Zw82DPZU&lat=" + 10.1 + "&lng=" + 10.2); // DLSALKJDSBALKJGBALSKDJBFKLAJGFLKSLKJGFLAJKGF
+			var user_pos = new google.maps.Marker({
+				map: map, title: 'you!', position: user
+			});
 
-		var user_pos = new google.maps.Marker({
-			map: map, title: 'you!', position: user
-		});
 
-		var request = new XMLHttpRequest();
-		request.open("POST", "https://hans-moleman.herokuapp.com/rides");
-		request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-		request.send("username=IZ1sLGwg&lat=" + user_lat + "&lng=" + user_lng);
 
-		request.onreadystatechange = function() {
-			console.log("initting cars");
-			if (request.status == 200 && request.readyState == 4) {
-				console.log(request.responseText); // DELETE LATER HGVKJHDBAOJGKDJXBALGJFBGLJFDABZLJKGNFALJFGDJFABN
-				cars = JSON.parse(request.responseText);
-				dispCars();
-				var path = [ ];
-				path.push(user); path.push(new google.maps.LatLng(closest_car['car']['lat'], closest_car['car']['lng'])) ;
+			var request = new XMLHttpRequest();
+			request.open("POST", "https://hans-moleman.herokuapp.com/rides");
+			request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+			request.send("username=Zw82DPZU&lat=" + user_lat + "&lng=" + user_lng);
 
-				var line = new google.maps.Polyline({
-					map: map, path: path, 
-					strokeColer: '#000080', strokeOpacity: 0.065, strokeWeight: 1
-				})
+			request.onreadystatechange = function() {
+				console.log("initting cars");
+				if (request.status == 200 && request.readyState == 4) {
+					console.log(request.responseText); // DELETE LATER HGVKJHDBAOJGKDJXBALGJFBGLJFDABZLJKGNFALJFGDJFABN
+					cars = JSON.parse(request.responseText);
+					dispCars();
+					var path = [ ];
+					path.push(user); path.push(new google.maps.LatLng(closest_car['car']['lat'], closest_car['car']['lng'])) ;
 
-				google.maps.event.addListener(user_pos, 'click', function() {
-					infowindow.setContent("Closest Vehicle: " + cars[closest_car['car']]['username'] + "; Distance: " + closest_car['distance'] + "miles");
-					infowindow.open(map, user_pos);
-				});
+					console.log("making polyline");
+					var line = new google.maps.Polyline({
+						map: map, path: path, 
+						strokeColer: '#203e6e', strokeOpacity: 0.65, strokeWeight: 2
+					})
+
+					google.maps.event.addListener(user_pos, 'click', function() {
+						infowindow.setContent("Closest Vehicle: " + cars[closest_car['car']]['username'] + "; Distance: " + closest_car['distance'] + "miles");
+						infowindow.open(map, user_pos);
+					});
+				}
 			}
-		}
+		});
 	}
 	else {
 		alert("No geolocation. This won't work :/");
@@ -82,14 +70,14 @@ function dispCars() {
 
 	for(var i = 0; i < num_cars; i++){
 		car_latlng = new google.maps.LatLng(cars[i].lat, cars[i].lng);
-		aux_distance = google.maps.geometry.spherical.computeDistanceBetween(user, car_latlng);
+		aux_distance = 0.000621371 * google.maps.geometry.spherical.computeDistanceBetween(user, car_latlng); // 0.000621 converts meters to miles
 		car_marker = new google.maps.Marker({
 			position: car_latlng,
 			title: cars[i]['username'] + " is " + aux_distance + " miles away",
 			icon: car_pic,
 			map: map
 		});
-		if(aux_distance < distance){
+		if(aux_distance < closest_car['distance']){
 			closest_car['car'] = cars[i]; closest_car['distance'] = aux_distance;
 		}
 		google.maps.event.addListener(car_marker, "click", function() {
